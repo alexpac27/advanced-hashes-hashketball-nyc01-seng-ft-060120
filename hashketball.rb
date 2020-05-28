6460ef851c920aba2ea5a8089c45139308bac7c9
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-28 23:48:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -204,7 +204,7 @@ def player_stats(player_name)
   game_hash.each do |team, team_details_hash|
     team_details_hash[:players].each do |stats|
 
-      if stats[:name] == player_name
+      if stats[:player_name] == player_name
         stats.delete(:name)
         player_stats = stats
       end
